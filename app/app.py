93142b9142ab29dfdd9742b5f732fec5dadd9f26
Hirotaro Sagi <hirotaro.ifps@yahoo.com>
2023-12-01 08:31:14 +0900

    temporary commit

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
+from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify, render_template
-from openai import OpenAI
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 load_dotenv()
-
 API_KEY = os.environ.get("OPENAI_API_KEY")
-
 if not API_KEY:
     raise RuntimeError("OpenAI API key is missing.")
 
@@ -20,16 +20,20 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/send", methods=["POST"])
-def send():
-    data = request.json
-
+@socketio.on("send_message")
+def handle_message(data):
     messages = data["messages"]
 
-    response = client.chat.completions.create(model="gpt-4-1106-preview", messages=messages)
+    stream = client.chat.completions.create(
+        model="gpt-4",
+        messages=messages,
+        stream=True,
+    )
 
-    return jsonify(reply=response.choices[0].message.content)
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            socketio.emit("receive_message", {"text": chunk.choices[0].delta.content})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
